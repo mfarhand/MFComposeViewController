7d38510aa4c50073d37ec7b16f9ceb081d77b32b
Mohamad Farhand <m.farhand@imenpardis.com>
2015-10-11 11:11:22 +0330

    Initial Commit

---
+++ MFComposeViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MFComposeViewController
+//
+//  Created by Mohamad Farhand on 10/11/15.
+//  Copyright © 2015 ideveloper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MFComposeViewController/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MFComposeViewController
+//
+//  Created by Mohamad Farhand on 10/11/15.
+//  Copyright © 2015 ideveloper. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
